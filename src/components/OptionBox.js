b6a1dba7d382de2af9844e5d321c9f51cbc9a745
chelotasnim <grumpfyxylozx@gmail.com>
2024-02-24 17:36:47 +0700

    feature: skeleton iamge

--- src/components/OptionBox.js
+++ src/components/OptionBox.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import '../styles/component-styles/box-option.css';
+import SkeletonImg from './SkeletonImg';
 
 const OptionBox = (props) => {
+    const [load, setLoad] = useState(false);
+
+    useEffect(() => {
+        if (props.img) {
+            const image = new Image();
+            image.src = props.img;
+            image.onload = () => setLoad(true);
+        }
+    }, [props.img]);
+
     return (
-        <div className={`box-option ${props.active === true && 'active'}`}>
-            <div className='icon'>
-                <img src={props.img} draggable='false' />
+        load === true ?
+            <div className={`box-option ${props.active === true && 'active'}`}>
+                <div className='icon'>
+                    <img src={props.img} draggable='false' />
+                </div>
+                <p>{props.value}</p>
             </div>
-            <p>{props.value}</p>
-        </div>
+            :
+            <SkeletonImg />
     );
 };
 
---
+++ src/components/SkeletonImg.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import '../styles/component-styles/box-option.css';
+
+const SkeletonImg = () => {
+    return (
+        <div className='skeleton'>
+        </div>
+    )
+}
+
+export default SkeletonImg;
